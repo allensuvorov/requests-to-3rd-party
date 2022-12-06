7b4d7fd7e197731abfffd64ec4ffbf18536af319
Allen <allenvs@mail.ru>
2022-12-06 23:14:34 +0600

    main.go initial code, object with layers

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,18 @@
 package main
 
-import "log"
+import (
+	"jobsearch/companies/axxonsoft/projects/3rdparty/internal/remote/routers"
+	"log"
+	"net/http"
+)
 
 func main() {
+	ReqStorage := storage.NewReqStorage()
+	ReqService := service.NewReqService(ReqStorage)
+	ReqHandler := handlers.NewReqHandler(ReqService)
+	r := routers.NewRouter(ReqHandler)
 	sa := ":8080"
 	log.Println("Serving on port", sa)
+	log.Fatal(http.ListenAndServe(sa, r))
+
 }
